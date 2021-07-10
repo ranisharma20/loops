64dcbef33d28c1f3d7b18292404325b35ef39a47
ranisharma20 <84247684+ranisharma20@users.noreply.github.com>
2021-07-10 20:16:43 +0530

    Add files via upload

---
+++ harshad_number.py
@@ -0,0 +1,13 @@
+n=int(input("enter the any number"))
+sum=0
+c=n
+r=0
+while c>0:
+    r=c%10
+    sum=sum+r
+    c=c//10
+if n%sum==0:
+    print('Harshad number')
+else:
+    print('not hardhad number')
+
